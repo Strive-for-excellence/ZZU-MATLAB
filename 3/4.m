8ded7ebf1f197c66a44aac0ebef59d34a9d96103
Strive-for-excellence <1505656319@qq.com>
2018-05-23 17:13:15 +0800

    Create 4.m

---
+++ 3/4.m
@@ -0,0 +1,10 @@
+dt= 0.01; t = -1:dt:3;
+f1 = Heaviside(t) - Heaviside(t-1);
+f2 = f1;
+f = conv(f1,f2)*dt; n = length(f); tt = (0:n-1)*dt-2;
+subplot(221),plot(t,f1),grid on;
+axis([-1,3,-0.2,1.2]);title('f1(t)'); xlabel('t');
+subplot(222),plot(t,f2),grid on;
+axis([-1,3,-0.2,1.2]); title('f2(t)'); xlabel('t');
+subplot(223);plot(tt,f),grid on;
+title('f(t)  = f1(t)*f2(t)'); xlabel('t');
